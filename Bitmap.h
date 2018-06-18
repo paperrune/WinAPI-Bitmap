b48fe1fb6efa1f3232cd078568c4f5ca9cd6521c
paperrune <paperrune@naver.com>
2018-06-19 02:21:04 +0900

    Update Bitmap.h

--- Bitmap.h
+++ Bitmap.h
@@ -1,22 +1,22 @@
 #include <Windows.h>
 
-class Bitmap{
+class Bitmap {
 private:
-	unsigned char *pixel;
-
 	int pad;
 
 	BITMAPINFO bmInfo;
-
-	BITMAPINFOHEADER bmInfoHeader;
 
 	HBITMAP hBitmap;
 
 	HDC hScrDC;
 	HDC hMemDC;
 public:
+	unsigned char *pixel;
+
 	int width;
 	int height;
+
+	BITMAPINFOHEADER bmInfoHeader;
 
 	Bitmap();
 	Bitmap(const char path[]);
@@ -27,9 +27,12 @@ public:
 	void fromFile(const char path[]);
 	void fromScreen();
 	void fromScreen(int reference_x, int reference_y, int screen_width, int screen_height);
+	void resize(int width, int height);
 	void rotate(double angle);
+	void setPixel(int x, int y, int BGR, unsigned char value);
 	void toFile(const char path[]);
 
-	void setPixel(int x, int y, int BGR, unsigned char value);
 	unsigned char getPixel(int x, int y, int BGR);
+
+	Bitmap* copy();
 };
